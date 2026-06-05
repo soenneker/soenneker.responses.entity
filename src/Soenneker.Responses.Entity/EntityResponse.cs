281f81bc099df95d412c05e65a440c6ae9380d79
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:37:24 -0500

    xml commenting

--- src/Soenneker.Responses.Entity/EntityResponse.cs
+++ src/Soenneker.Responses.Entity/EntityResponse.cs
@@ -10,12 +10,21 @@ namespace Soenneker.Responses.Entity;
 [PublicOpenApiObject]
 public record EntityResponse
 {
+    /// <summary>
+    /// The unique identifier for the entity.
+    /// </summary>
     [JsonPropertyName("id")]
     public virtual string Id { get; set; } = null!;
 
+    /// <summary>
+    /// The date and time when the entity was created.
+    /// </summary>
     [JsonPropertyName("createdAt")]
     public virtual DateTimeOffset CreatedAt { get; set; }
 
+    /// <summary>
+    /// The date and time when the entity was last modified, if applicable.
+    /// </summary>
     [JsonPropertyName("modifiedAt")]
     public virtual DateTimeOffset? ModifiedAt { get; set; }
 }
